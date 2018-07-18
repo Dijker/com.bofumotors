1c2f6d742e414fb189600a0f7c7f590c80acb106
Matthew Waanders <matthew@athom.nl>
2018-07-18 14:09:49 +0200

    fixed typo

--- drivers/bofu/driver.js
+++ drivers/bofu/driver.js
@@ -7,7 +7,7 @@ const util = RFDriver.util;
 module.exports = class BofuDriver extends RFDriver.Driver {
 
     onRFInit() {
-        const myFlowAction = new Homey.FlowCardAction('tilt_bofu').register()
+        const myFlowAction = new Homey.FlowCardAction('my_bofu').register()
             .registerRunListener( (args, state) => {
                 args.device.send({cmd: 'my', windowcoverings_state: 'idle'});
             });
